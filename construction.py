d3f8083f56a79326adc0597c2d57bbfa5087477c
lightbourn/patrick <lightbourn.p@gmail.com>
2024-09-03 19:12:05 -0600

    Add files via upload

---
+++ construction.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#Daughter's room measurements:
+#width and length is 13 x 13 feet
+width = 13
+length = 13
+perimeter =2*(width+length) 
+print(perimeter)
+area = length*width
+print(area)
+print("Floor material that is required to cover the entire floor is:",area,"square feet")
+#Print states 169 square feet
+print("Baseboard material that is required for the room is",perimeter, "feet")
