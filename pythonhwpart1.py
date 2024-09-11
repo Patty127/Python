c473da8b9410ad3945c537a925c7f169d588aecf
lightbourn/patrick <lightbourn.p@gmail.com>
2024-09-11 09:44:54 -0600

    Add files via upload

---
+++ pythonhwpart1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#Enter input that asks for age
+x=input("Would you provide your age in years?: ")
+#age_days variable = int(x)*365
+age_days=int(x)*365
+#age_months variable
+age_months=int(x)*12
+#age_hours variable
+age_hours=age_days*24
+#Instructions state to print age_days variable
+print("Your age in days is:", age_days)
+#Second print command is age_months variable
+print("Your age in months is:", age_months)
+#Last print command is age_hours variable
+print("Your age in hours is:", age_hours) 
---
+++ pythonhwpart2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+x=input("Hello. Please enter an integer value of your choice: ")
+x=int(x)
+y = x * x + 7 * x - 77 * x - 777 * x
+print("The value based on your input of x =",x,"would be equal to",y)
